161e48b00deca4472e954c007c726b39ec967438
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-17 03:48:15 -0300

    Add context to application to monitor page and layout

---
+++ src/context/store.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { createContext, useContext, Dispatch, SetStateAction, useState } from 'react';
+
+// interface ContextProps {
+//     page: 'home' | 'academic' | 'professional' | 'personal' | 'certificates' | 'projects';
+//     setPage: Dispatch<SetStateAction<'home' | 'academic' | 'professional' | 'personal' | 'certificates' | 'projects'>>;
+//     language: 'pt' | 'en';
+//     setLanguage: Dispatch<SetStateAction<'pt' | 'en'>>;
+// }
+
+interface ContextProps {
+    page: string;
+    setPage: (page: string) => void;
+    language: string;
+    setLanguage: (language: string) => void;
+}
+
+const GlobalContext = createContext<ContextProps>({
+    page: 'home',
+    language: 'pt'
+} as ContextProps)
+
+export const GlobalContextProvider = ({ children } : any) => {
+    const [page, setPage] = useState('home');
+    const [language, setLanguage] = useState('pt');
+
+    return (
+        <GlobalContext.Provider value={{ page, setPage, language, setLanguage }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(GlobalContext);
