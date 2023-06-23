c57bac21cde3efc3948bb3b23aa940b2d21cd39f
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-23 15:34:38 -0300

    Add personal timeline

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,7 +1,16 @@
+import GeneralTimeline from '@/components/GeneralTimeline'
+
 export default function Personal() {
   return (
-    <>
-      <h1>Personal</h1>
-    </>
+    <div className="flex flex-col py-20 pl-20 font-sans">
+      <GeneralTimeline
+        startDate={'AGO 2015'}
+        endDate={'JAN 2016'}
+        title={'Intercâmbio para o Canadá'}
+        subtitle={'Ecole Cariboo Hill Secondary School'}
+        description={['a', 'b', 'c', 'a', 'b', 'c']}
+        color={'bg-my-blue'}
+      />
+    </div>
   )
 }
