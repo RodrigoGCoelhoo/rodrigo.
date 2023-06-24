76ceb1ad791f181e48dd59ff3ec38bcfe1bd58cb
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-23 22:47:26 -0300

    Add projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,25 @@
+import GeneralCard from '@/components/GeneralCard'
+
+const a = [
+  { title: 'A', content: 'B' },
+  { title: 'C', content: 'D' },
+  { title: 'E', content: 'F' },
+  { title: 'G', content: 'H' },
+]
+
 export default function Projects() {
   return (
-    <>
-      <h1>Projects</h1>
-    </>
+    <div className="mt-20 flex h-full w-full items-center justify-center font-sans">
+      <div className="grid h-full w-[80%] grid-cols-3 gap-6 ">
+        {a.map((item, index) => (
+          <GeneralCard
+            key={index}
+            title={item.title}
+            content={item.content}
+            borderColor={'border-my-purple'}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
