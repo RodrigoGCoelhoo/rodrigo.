196a53d8a31f0b60f16381bc40d29ba300f3a0aa
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-27 00:31:50 -0300

    Implement projects page with real dat

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,24 +1,28 @@
-import GeneralCard from '@/components/GeneralCard'
+'use client'
 
-const a = [
-  { title: 'A', content: 'B' },
-  { title: 'C', content: 'D' },
-  { title: 'E', content: 'F' },
-  { title: 'G', content: 'H' },
-]
+import GeneralCard from '@/components/GeneralCard'
+import { useGlobalContext } from '@/context/store'
+
+import projectsData from '@/data/projects.json'
 
 export default function Projects() {
+  const { language } = useGlobalContext()
+
   return (
     <div className="mt-20 flex h-full w-full items-center justify-center font-sans">
-      <div className="grid h-full w-[80%] grid-cols-3 gap-6 ">
-        {a.map((item, index) => (
-          <GeneralCard
-            key={index}
-            title={item.title}
-            content={item.content}
-            borderColor={'border-my-purple'}
-          />
-        ))}
+      <div className="grid h-full w-[90%] grid-cols-3 gap-10 ">
+        {projectsData.map((data, index) => {
+          const languageData = data[language as keyof typeof data]
+
+          return (
+            <GeneralCard
+              key={index}
+              title={languageData.title}
+              content={languageData.shortDescription}
+              borderColor={'border-my-purple'}
+            />
+          )
+        })}
       </div>
     </div>
   )
