b1bf8f01a38ecfedbf90f597c6611565aa5b1ad1
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-26 23:53:00 -0300

    Implement data view on timeline for academic page

--- src/app/academic/page.tsx
+++ src/app/academic/page.tsx
@@ -1,27 +1,31 @@
+'use client'
+
 import GeneralTimeline from '@/components/GeneralTimeline'
+import { useGlobalContext } from '@/context/store'
+
+import academicData from '@/data/academic.json'
 
 export default function Academic() {
+  const { language } = useGlobalContext()
+
   return (
     <div className="flex flex-col py-20 pl-20 font-sans">
-      <GeneralTimeline
-        startDate="JAN 2020"
-        endDate="DEC 2024"
-        title="Insper"
-        subtitle="Engenharia da computação"
-        // description={['a', 'b', 'c', 'a', 'b', 'c']}
-        color={'bg-my-red'}
-        continuous={false}
-      />
+      {academicData.map((data, key) => {
+        const languageData = data[language as keyof typeof data]
 
-      <GeneralTimeline
-        startDate="JAN 2020"
-        endDate="DEC 2024"
-        title="Insper"
-        subtitle="Engenharia da computação"
-        description={['a', 'b', 'c', 'a', 'b', 'c']}
-        color={'bg-my-red'}
-        continuous={false}
-      />
+        return (
+          <GeneralTimeline
+            key={key}
+            startDate={languageData.startDate}
+            endDate={languageData.endDate}
+            title={languageData.title}
+            subtitle={languageData.subtitle}
+            description={languageData.description}
+            color={'bg-my-red'}
+            continuous={languageData.continuous}
+          />
+        )
+      })}
     </div>
   )
 }
