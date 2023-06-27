0a66cc5f0120fe0c8872b47abdc8617fb859d20a
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2023-06-27 00:32:45 -0300

    Style changes for the projects cards

--- src/components/GeneralCard/index.tsx
+++ src/components/GeneralCard/index.tsx
@@ -11,10 +11,10 @@ export default function GeneralCard({
 }: GeneralCardProps) {
   return (
     <div
-      className={`inline-block h-[150px] w-full overflow-hidden border-l-8 ${borderColor} bg-light-gray px-5 py-4 duration-200 ease-in-out hover:scale-105`}
+      className={`inline-block min-h-[150px] w-full overflow-hidden border-l-8 ${borderColor} bg-light-gray px-6 py-5 duration-200 ease-in-out hover:scale-105`}
     >
-      <div className="mb-2 text-sm font-medium">{title}</div>
-      <div className="line-clamp-5 whitespace-normal text-sm font-light leading-4">
+      <div className="mb-4 font-medium">{title}</div>
+      <div className="line-clamp-5 whitespace-normal text-sm font-light leading-5">
         {content}
       </div>
     </div>
